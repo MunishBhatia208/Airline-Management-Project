011daf63b3d0508e3e7823046c53887f68de7911
MunishBhatia208 <118382959+MunishBhatia208@users.noreply.github.com>
2024-05-18 20:38:07 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './assets/components/Navbar'
 const App = () => {
   return (
     <>
-      <img src="backgroundimage.jpg" width = "100%" className='overflow-scroll fixed' alt="" />
+      <img src="backgroundimage.jpg" width = "100%"  className='overflow-scroll fixed' alt="" />
       <div className='relative top-0'>
           <Navbar/>
       </div>
