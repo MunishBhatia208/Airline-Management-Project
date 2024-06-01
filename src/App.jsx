1cb82e9a8563bcf5e8590a0d22f74b9b5319ceec
MunishBhatia208 <118382959+MunishBhatia208@users.noreply.github.com>
2024-06-01 16:08:35 +0530

    searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,18 @@ import Fsb from './assets/components/fsb'
 const App = () => {
   return (
     <>
-    <div className='relative min-h-96'>
-      <div>
-      <img src="src\assets\images\backgroundimage.jpg" width = "100%"  className='absolute' alt="" />
-      </div>
-      <div className='fixed top-0 min-w-full'>
+      <div className=' min-h-96 relative top-0'>
+        <div>
+          <img src="src\assets\images\backgroundimage.jpg" width="100%" className='absolute' alt="" />
+        </div>
+        <div className='min-w-full relative top-0'>
           <Navbar/>
+        </div>
+        <div className='relative top-0 left-[1040px] w-[450px] rounded-2xl bg-slate-300 py-5'>
+          <Fsb />
+        </div>
       </div>
-      <div className='top-20 relative '>
-          <Fsb/>
-      </div>
-      </div>
-    
-  </>
+    </>
   )
 }
 export default App
--- src/assets/components/fsb.jsx
+++ src/assets/components/fsb.jsx
@@ -1,60 +1,65 @@
-import React from 'react'
-import Countriesdropdown from './countriesdropdown'
+import React,{ useState } from 'react'
 const fsb = () => {
+    const [form, setForm] = useState({source: "", destination: "", checkin: "", checkout: "", economy: "", travellers: ""})
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+    let flightDetails;
     return (
         <>
-            <div className='bg-slate-600'>
-                <div className='right-0 fixed w-4/12  my-7 min-h-96 px-5 text-center text-3xl font-bold'>Search your flights</div>
-                <div className='right-0 fixed w-4/12  my-16 min-h-96 px-5'>
-                    <div className="container  grid grid-cols-2 gap-7 my-6">
-                        <div className="border-black border-2 w-full">
+            <div>
+                <div className='w-full px-5 text-center text-3xl font-bold'>Search your flights</div>
+                <div className='w-full px-5 '>
+                    <div className="container grid grid-cols-2 gap-7 my-6">
+                        <div className="border-black border-2 w-full rounded-xl relative">
                             <div className="px-2">From</div>
-                            <div className="flex justify-between text-center items-center ">
-                                <img src="src\assets\images\downarrow.svg" className='px-3' alt="" />
+                            <div className="flex justify-between text-center items-center relative">
+                                <input onChange={handleChange} value={form.source} type="text" placeholder='Source' className='bg-slate-300 max-w-36 rounded-xl border-none' name='source' />
+                                <img src="src\assets\images\downarrow.svg" className='cursor-pointer px-3' alt="" />
                             </div>
                         </div>
-                        <div className="border-black border-2 w-full">
+                        <div className="border-black border-2 w-full relative rounded-xl">
                             <div className="px-2">To</div>
                             <div className="flex justify-between text-center items-center ">
-                                <div className="px-6 py-2 text-xl">England</div>
-                                <img src="src\assets\images\downarrow.svg" className='px-3' alt="" />
+                                <input onChange={handleChange} value={form.destination} type="text" placeholder='Destination' className='bg-slate-300 max-w-36 rounded-xl border-none' name='destination' />
+                                <img src="src\assets\images\downarrow.svg" className='cursor-pointer px-3' alt="" />
                             </div>
                         </div>
-                        <div className="border-black border-2 w-full">
+                        <div className="border-black border-2 w-full rounded-xl">
                             <div className="px-2">Check-in</div>
                             <div className="flex justify-between text-center items-center ">
-                                <div className="px-6 py-2 text-xl">England</div>
-                                <img src="src\assets\images\downarrow.svg" className='px-3' alt="" />
+                                <input onChange={handleChange} value={form.checkin} type="text" placeholder='Check-in' className='bg-slate-300 max-w-36 rounded-xl border-none' name='checkin' />
+                                <img src="src\assets\images\downarrow.svg" className='px-3 cursor-pointer' alt="" />
                             </div>
                         </div>
-                        <div className="border-black border-2 w-full">
+                        <div className="border-black border-2 w-full rounded-xl">
                             <div className="px-2">Check-Out</div>
                             <div className="flex justify-between text-center items-center ">
-                                <div className="px-6 py-2 text-xl">England</div>
-                                <img src="src\assets\images\downarrow.svg" className='px-3' alt="" />
+                                <input onChange={handleChange} value={form.checkout} type="text" placeholder='Check-out' className='bg-slate-300 max-w-36 rounded-xl border-none' name='checkout' />
+                                <img src="src\assets\images\downarrow.svg" className='px-3 cursor-pointer' alt="" />
                             </div>
                         </div>
-                        <div className="border-black border-2 w-full">
+                        <div className="border-black border-2 w-full rounded-xl">
                             <div className="px-2">Class</div>
                             <div className="flex justify-between text-center items-center ">
-                                <div className="px-6 py-2 text-xl">England</div>
-                                <img src="src\assets\images\downarrow.svg" className='px-3' alt="" />
+                                <input onChange={handleChange} value={form.economy} type="text" placeholder='economy' className='bg-slate-300 max-w-36 rounded-xl border-none' name='economy'/>
+                                <img src="src\assets\images\downarrow.svg" className='px-3 cursor-pointer' alt="" />
                             </div>
                         </div>
-                        <div className="border-black border-2 w-full">
+                        <div className="border-black border-2 w-full rounded-xl">
                             <div className="px-2">Travellers</div>
                             <div className="flex justify-between text-center items-center ">
-                                <div className="px-6 py-2 text-xl">England</div>
-                                <img src="src\assets\images\downarrow.svg" className='px-3' alt="" />
+                                <input onChange={handleChange} value={form.travellers} type="text" placeholder='Travellers' className='bg-slate-300 max-w-36 rounded-xl border-none' name='travellers' />
+                                <img src="src\assets\images\downarrow.svg" className='px-3 cursor-pointer' alt="" />
                             </div>
                         </div>
                         <div className="col-span-2 flex justify-between items-center border-black border-2 w-full">
                             <div className="flex items-center space-x-2 px-6 py-2">
-                                <input type="radio" className="form-checkbox h-5 w-5 text-blue-600" name="g" />
+                                <input type="radio" className="form-checkbox h-5 w-5 text-blue-600" name="g" defaultValue={1} />
                                 <span>ONE WAY</span>
                             </div>
                             <div className="flex items-center space-x-2 px-6 py-2">
-                                <input type="radio" className="form-checkbox h-5 w-5 text-blue-600" name="g" />
+                                <input type="radio" className="form-checkbox h-5 w-5 text-blue-600" name="g" defaultValue={0} />
                                 <span>ROUND TRIP</span>
                             </div>
                         </div>
